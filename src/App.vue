9947899b915f1a27dcfef9b64b6aeba45e381e28
axin1007 <804871168@qq.com>
2022-07-19 10:57:40 +0800

    第五天课堂

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <UserAxios></UserAxios>
+  </div>
+</template>
+
+<script>
+import UserAxios from "./components/UserAxios.vue";
+export default {
+  components:{
+    UserAxios,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from 'axios'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  beforeCreate(){
+    Vue.prototype.$axios = axios
+  }
 }).$mount('#app')
